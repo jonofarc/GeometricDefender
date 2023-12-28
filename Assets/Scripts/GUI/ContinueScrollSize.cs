bd24b57806be8b4c40c1c4f52a46e2c6e8b5dd5e
unknown <jonathan.maldonado@exprealty.net>
2023-12-28 15:35:00 -0600

    geneerate continue level buttons dinamycally

--- Assets/Scripts/GUI/ContinueScrollSize.cs
+++ Assets/Scripts/GUI/ContinueScrollSize.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.UI;
 
 public class ContinueScrollSize : MonoBehaviour
 {
     public TextAsset CreepSpawnList;
+    public GameObject levelGUIButton;
     public float itemSize = 3;
     public float margingTop = 50;
+    public float HeightSeparation = 130;
     private List<string> FileLines;
 
 
@@ -53,7 +55,50 @@ public class ContinueScrollSize : MonoBehaviour
         
         // Change the height to a new value (e.g., 200 units)
         ChangeHeightValue((((LevelCount-9)/3)* itemSize)+margingTop);
-        
+        CreateLevelButtons(LevelCount);
+
+    }
+
+    void CreateLevelButtons(int buttonAmount)
+    {
+        //All of this logic could probably be solkved in a better way but at this piont this is the first solution that comes to mind
+        int YMultiplier = 0;
+        int YIncreaseFlag = 0;
+        for (int i =1; i< buttonAmount+1; i++) {
+            GameObject duplicatedObject = Instantiate(levelGUIButton);
+
+            // Access RectTransforms for positioning in UI space
+            RectTransform parentRectTransform = levelGUIButton.GetComponent<RectTransform>();
+            RectTransform childRectTransform = duplicatedObject.GetComponent<RectTransform>();
+
+            // Set the parent of the childElement to be the parentElement
+            childRectTransform.SetParent(parentRectTransform.parent, false); // Set the second parameter to true to maintain local position
+
+            duplicatedObject.name = "Level"+i;
+            Text textComponent = duplicatedObject.GetComponentInChildren<Text>();
+            textComponent.text = i.ToString();
+
+            // Optionally, you can set the position, rotation, and other properties of the duplicated object
+            if (i % 2 == 0 && i % 3  != 0) // If i is even
+            {
+                duplicatedObject.transform.localPosition = new Vector3(parentRectTransform.localPosition.x+325.0f, parentRectTransform.localPosition.y - (YMultiplier * 130), 0f);
+            }else if (i % 3 == 0) // If i is even
+            {
+                duplicatedObject.transform.localPosition = new Vector3(parentRectTransform.localPosition.x+650.0f, parentRectTransform.localPosition.y - (YMultiplier * 130), 0f);
+            }
+            else // If i is odd
+            {
+                duplicatedObject.transform.localPosition = new Vector3(parentRectTransform.localPosition.x, parentRectTransform.localPosition.y-(YMultiplier*130), 0f); 
+            }
+
+            YIncreaseFlag++;
+            if (YIncreaseFlag >= 3) {
+                YMultiplier++;
+                YIncreaseFlag = 0;
+            }
+
+        }
+        levelGUIButton.SetActive(false);
     }
 
     void ChangeHeightValue(float newHeight)
